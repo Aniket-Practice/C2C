96a5b4545a8af131ad3cc1d204ca744a08b3d88e
Aniket Practice <waghmareaniket150@gmail.com>
2024-03-11 21:53:09 +0530

    Insufficiant Information

---
+++ AniketGym.java
@@ -0,0 +1,12 @@
+package C2C;
+
+/**
+ * AniketGym
+ */
+public class AniketGym {
+
+    public static void main(String[] args) {
+        System.out.println("Hey");
+        System.out.println("Bue");
+    }
+}
